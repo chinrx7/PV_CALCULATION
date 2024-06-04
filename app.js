c5961aa4c2c64bdfea9c4c8fbca21ed0753e158c
SATSERVER-02\SATAdmin <chinaphatr@gmail.com>
2024-06-04 15:25:53 +0700

    zz

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const schedule  = require('node-schedule');
 const logger = require('./middleware/logger');
+const parser = require('expression-parser/parse');
+const valuer = require('expression-parser/values');
 
 logger.loginfo('app start');
+
+const ast = parser('((a*b)/c)+d');
+
+const val = valuer.fromAST(ast)({a:10,b:7,c:4,d:1});
+
+console.log(val[0])
 
 const rule = new schedule.RecurrenceRule();
 
